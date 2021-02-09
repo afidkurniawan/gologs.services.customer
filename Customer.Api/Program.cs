2cf91162efee8784ac7bc10fb7930ab7698c7754
IqbalN <iqbal.nirbito+atlassian@gmail.com>
2021-02-09 12:42:46 +0700

    Added Serilog.
    # Conflicts:
    #	GoLogs.Services.Customer.sln.DotSettings

--- Customer.Api/Program.cs
+++ Customer.Api/Program.cs
@@ -4,16 +4,38 @@
 // Unauthorized copying of this file is strictly prohibited.
 // -------------------------------------------------------------
 
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace GoLogs.Services.Customer.Api
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class Program
     {
+        private static readonly string EnvironmentName;
+        private static readonly IConfiguration _configuration;
+
+        static Program()
+        {
+            EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(_configuration)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
